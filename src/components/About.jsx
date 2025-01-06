f98aa8cd1cfe65ea54644f3683dc54377df418cc
hmquann <mquan171@gmail.com>
2025-01-06 14:23:44 +0700

    update portfolio

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,24 +1,42 @@
-import aboutImg from "../assets/about.jpg"
-import { ABOUT_TEXT } from "../constants"
+import aboutImg from "../assets/about.jpg";
+import { ABOUT_TEXT } from "../constants";
+import { motion } from "motion/react";
 
 const About = () => {
   return (
-        <div className="border-b border-neutral-900 pb-4">
-            <h1 className="my-20 text-center text-4xl">About Me</h1>
-            <div className="flex flex-wrap ">
-                <div className="w-full lg:w-1/2 lg:p-8">
-                    <div className="flex items-center justify-center">
-                        <img className="rounded-lg" src={aboutImg} alt="" />
-                    </div>
-                </div>
-                <div className="w-full lg:w-1/2 lg:p-8 ">
-                   <div className="flex lg:justify-start justify-center">
-                   <div className="py-6 max-w-xl my-2">{ABOUT_TEXT}</div>
-                   </div>
-                </div>
-            </div>
+    <div className="border-b border-neutral-900 pb-4">
+      <motion.h1
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 1 }}
+        className="my-20 text-center text-4xl"
+      >
+        About Me
+      </motion.h1>
+      <div className="flex flex-wrap ">
+        <div className="w-full lg:w-1/2 lg:p-8">
+          <motion.div
+            whileInView={{ opacity: 1, x: 0 }}
+            initial={{ opacity: 0, x: -100 }}
+            transition={{ duration: 1 }}
+            className="flex items-center justify-center"
+          >
+            <img className="rounded-lg" src={aboutImg} alt="" />
+          </motion.div>
         </div>
-  )
-}
+        <div className="w-full lg:w-1/2 lg:p-8 ">
+          <motion.div
+            whileInView={{ opacity: 1, x: 0 }}
+            initial={{ opacity: 0, x: 100 }}
+            transition={{ duration: 1 }}
+            className="flex lg:justify-start justify-center"
+          >
+            <div className="py-6 max-w-xl my-2">{ABOUT_TEXT}</div>
+          </motion.div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default About
+export default About;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,26 @@
-import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa"
-import logo from "../assets/kevinRushLogo.png"
-import { FaSquareXTwitter } from "react-icons/fa6"
+import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
+import logo from "../assets/kevinRushLogo.png";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { motion } from "motion/react";
 
 const Navbar = () => {
   return (
     <nav className=" mb-20 flex items-center justify-between py-6">
-        <div className="flex flex-shrink-0 items-center">
-            <img className="mx-2 w-10" src={logo} alt="" />
-        </div>
-        <div className="m-8 flex items-center justify-center gap-4">
-            <FaLinkedin />
-            <FaGithub />
-            <FaInstagram />
-            <FaSquareXTwitter />
-        </div>
+      <div className="flex flex-shrink-0 items-center">
+        <img className="mx-2 w-10" src={logo} alt="" />
+      </div>
+      <motion.div
+        whileInView={{ opacity: 1.5, x: 0 }}
+        initial={{ opacity: 0, x: 100 }}
+        className="m-8 flex items-center justify-center gap-4"
+      >
+        <FaLinkedin />
+        <FaGithub />
+        <FaInstagram />
+        <FaSquareXTwitter />
+      </motion.div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
