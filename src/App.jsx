e37ca1009ae4373fa7014867918a50569c89ffdc
hmquann <mquan171@gmail.com>
2025-01-07 15:03:29 +0700

    update bg

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,9 @@ import Technology from "./components/Technology";
 
 const App = () => {
   return (
-    <div className="overflow-x-hidden text-neutral-400 antialiased selection:bg-cyan-300 selection:text-cyan-900">
+    <div className="overflow-x-hidden text-neutral-400  selection:bg-cyan-500 selection:text-cyan-900">
       <div className="fixed top-0 -z-10 h-full w-full">
-      <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_10%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       </div>
       <div className="container mx-auto px-8">
         <Navbar />
